fd89539710279f1c3cace91b6ca7a9c4d1e9ca9e
emre durgunlu <emredurgunlu@gmail.com>
2024-02-12 01:22:41 +0300

    authSlice for login

--- src/features/authSlice.jsx
+++ src/features/authSlice.jsx
@@ -1,11 +1,33 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: "",
+  loading: false,
+  error: false,
+  token: "",
+};
 
 const authSlice = createSlice({
   name: "auth",
+  initialState,
 
-  initialState: {},
-  reducers: {},
-})
+  reducers: {
+    fetchStart: (state) => {
+      state.loading = true;
+    },
+    loginSuccess: (state, { payload }) => {
+      state.loading = false;
+      state.user = payload.user.username;
+      state.token = payload.token;
+    },
+    registerSuccess:()=>{},
+    fetchFail: (state) => {
+      state.loading=false;
+      state.error=true;
 
-export const {} = authSlice.actions
-export default authSlice.reducer
+    },
+  },
+});
+
+export const { fetchStart, loginSuccess, fetchFail } = authSlice.actions;
+export default authSlice.reducer;
--- src/service/useAuthCalls.js
+++ src/service/useAuthCalls.js
@@ -1,29 +1,38 @@
 import axios from "axios";
 import { toastErrorNotify, toastSuccessNotify } from "../helper/ToastNotify";
 import { useNavigate } from "react-router-dom";
-
+import { fetchStart,loginSuccess,fetchFail } from "../features/authSlice";
+import { useDispatch } from "react-redux";
 const useAuthCalls = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
   const login = async (userInfo) => {
+    dispatch(fetchStart());
+
     try {
-      const data = await axios.post(
-        `${process.env.REACT_APP_BASE_URL}/auth/login`,
-        userInfo
-      );
-      toastSuccessNotify("Login işlemi basarili");
-      navigate("/stock");
-      console.log("login data", data.data);
-      // axios geleni data içine koyduğu için data.data şeklinde datayı çıkarmamız gerekiyor veya
-      // const {data} = await axios.post(
-      //   `${process.env.REACT_APP_BASE_URL}/auth/login`
+      // const data = await axios.post(
+      //   `${process.env.REACT_APP_BASE_URL}/auth/login`,
+      //   userInfo
       // );
       // console.log("login data", data.data);
+      // axios geleni data içine koyduğu için data.data şeklinde datayı çıkarmamız gerekiyor veya
+      const {data} = await axios.post(
+        `${process.env.REACT_APP_BASE_URL}/auth/login`
+      );
+      dispatch(loginSuccess(data));
+      toastSuccessNotify("Login işlemi basarili");
+      navigate("/stock");
+
     } catch (error) {
+      dispatch(fetchFail());
       toastErrorNotify("Login işlemi basarisiz");
       console.log(error);
     }
   };
-  return { login };
+  const register = async () => {};
+  const logout = async () => {};
+  return { login, logout, register };
 };
 
 export default useAuthCalls;
