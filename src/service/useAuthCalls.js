c9b4fdf088d620a70674e6600fd82aef7294fc0b
emre durgunlu <emredurgunlu@gmail.com>
2024-02-15 06:09:35 +0300

    axios instance

--- src/service/useAuthCalls.js
+++ src/service/useAuthCalls.js
@@ -1,7 +1,8 @@
-import axios from "axios";
+// import axios from "axios";
 import { toastErrorNotify, toastSuccessNotify } from "../helper/ToastNotify";
 import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+// import { useSelector } from "react-redux";
+import useAxios from "./useAxios";
 import {
   fetchStart,
   loginSuccess,
@@ -13,7 +14,8 @@ import { useDispatch } from "react-redux";
 const useAuthCalls = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const { token } = useSelector((state) => state.auth);
+  // const { token } = useSelector((state) => state.auth);
+  const { axiosWithToken, axiosPublic } = useAxios();
   const login = async (userInfo) => {
     dispatch(fetchStart());
 
@@ -24,10 +26,11 @@ const useAuthCalls = () => {
       // );
       // console.log("login data", data.data);
       // axios geleni data içine koyduğu için data.data şeklinde datayı çıkarmamız gerekiyor veya
-      const { data } = await axios.post(
-        `${process.env.REACT_APP_BASE_URL}/auth/login`,
-        userInfo
-      );
+      // const { data } = await axios.post(
+      //   `${process.env.REACT_APP_BASE_URL}/auth/login`,
+      //   userInfo
+      // );
+      const { data } = await axiosPublic.post("/auth/login/", userInfo);
       dispatch(loginSuccess(data));
       toastSuccessNotify("Login işlemi basarili");
       navigate("/stock");
@@ -40,11 +43,11 @@ const useAuthCalls = () => {
   const register = async (userInfo) => {
     dispatch(fetchStart());
     try {
-      const { data } = await axios.post(
-        `${process.env.REACT_APP_BASE_URL}/users/`,
-        userInfo
-      );
-      // const { data } = await axiosPublic.post("/users/", userInfo)
+      // const { data } = await axios.post(
+      //   `${process.env.REACT_APP_BASE_URL}/users/`,
+      //   userInfo
+      // );
+      const { data } = await axiosPublic.post("/users/", userInfo);
       dispatch(registerSuccess(data));
       navigate("/stock");
     } catch (error) {
@@ -54,11 +57,12 @@ const useAuthCalls = () => {
   const logout = async () => {
     dispatch(fetchStart()); // fetchStart bu satır olmasa da olur. logout işlemi için gerekli değil
     try {
-      await axios.get(`${process.env.REACT_APP_BASE_URL}/auth/logout`, {
-        headers: { Authorization: `Token ${token}` },
-      });
-      // await axiosWithToken("/auth/logout/");
-      // toastSuccessNotify("Çıkış işlemi başarili.");
+      // await axios.get(`${process.env.REACT_APP_BASE_URL}/auth/logout`, {
+      //   headers: { Authorization: `Token ${token}` },
+      // });
+      // Normalde logout post olur ama burda get olguğu için zaten default olarak axiosWithToken.get oluyor
+      await axiosWithToken("/auth/logout/");
+      toastSuccessNotify("Çıkış işlemi başarili.");
       dispatch(logoutSuccess());
       // navigate("/")
     } catch (error) {
